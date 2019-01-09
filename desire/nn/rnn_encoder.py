13a55d499646f1a85bd3f624c392e85c1572ad44
AkashGanesan <akaberto@gmail.com>
2019-01-09 05:13:49 -0500

    Made changes to make it the same as the DESIRE paper.

--- desire/nn/rnn_encoder.py
+++ desire/nn/rnn_encoder.py
@@ -1,35 +1,62 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from collections import namedtuple
+
+EncoderRNNParams = namedtuple('EncoderRNNParams', 'input_size intermediate_size gru_hidden_size n_layers dropout kernel_size')
+
+
+sample_parameters = EncoderRNNParams(input_size=2,
+                                     intermediate_size=20,
+                                     gru_hidden_size=48,
+                                     kernel_size=3,
+                                     n_layers=20,
+                                     dropout=0)
+
+
 
 
 class EncoderRNN(nn.Module):
     '''Source :
     https://pytorch.org/tutorials/beginner/chatbot_tutorial.html This will
     be used in the front end.
-    
+
     Removed the embedding layer as we it is directly encoded into positions (x,y)
     '''
-    def __init__(self, hidden_size, n_layers=1, dropout=0):
+    def __init__(self, params : EncoderRNNParams):
+        # hidden_size, n_layers=20, dropout=0
         super(EncoderRNN, self).__init__()
-        self.n_layers = n_layers
-        self.hidden_size = hidden_size
+        self.params = params
+        self.n_layers = params.n_layers
+        self.dropout = params.dropout
+        self.intermediate_size = params.intermediate_size
+        self.gru_hidden_size = params.gru_hidden_size
+        self.input_size = params.input_size
+        self.kernel_size = params.kernel_size
+
+        self.in_conv1d = nn.Conv1d(self.input_size,
+                                   self.intermediate_size,
+                                   self.kernel_size,
+                                   padding=True)
 
         # Initialize GRU; the input_size and hidden_size params are both set to 'hidden_size'
         #   because our input size is a word embedding with number of features == hidden_size
-        self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
-                          dropout=(0 if n_layers == 1 else dropout), bidirectional=True)
+        self.gru = nn.GRU(self.intermediate_size,
+                          self.gru_hidden_size,
+                          self.n_layers,
+                          dropout=(0 if self.n_layers == 1 else self.dropout),
+                          bidirectional=False,
+                          batch_first=True)
 
-    def forward(self, input_seq, input_lengths, hidden=None):
+
+    def forward(self, input_seq, hidden=None):
         # Pack padded batch of sequences for RNN module
-        packed = torch.nn.utils.rnn.pack_padded_sequence(input_seq, input_lengths)
+        # packed = torch.nn.utils.rnn.pack_padded_sequence(input_seq, input_lengths, batch_first=True)
+        packed = F.relu(self.in_conv1d(input_seq))
         # Forward pass through GRU
         outputs, hidden = self.gru(packed, hidden)
         # Unpack padding
-        outputs, _ = torch.nn.utils.rnn.pad_packed_sequence(outputs)
         # Sum bidirectional GRU outputs
-        outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:]
+        # outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:]
         # Return output and final hidden state
-        return outputs, hidden
-
-    
+        return outputs, hidden[-1]
