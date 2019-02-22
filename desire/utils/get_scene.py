f81a4e89f59f60e56ce1b0c10b0d986237c433b4
AkashGanesan <akaberto@gmail.com>
2019-02-21 23:06:21 -0500

    Fixed get_scene for relative positioning.

--- desire/utils/get_scene.py
+++ desire/utils/get_scene.py
@@ -1,15 +1,28 @@
 import torch
 
 
-def get_scene(scene, ypred):
+def get_scene(scene, ypred_rel, x):
+    '''get_scene
+    input
+    =====
+    scene: (W/2, H/2, x)  where x == 32
+    ypred: (x, y) where x, y are floats
+    output:
+    '''
+    z = ypred_rel + x
+    idx = z.long()
 
-    idx = ypred.long()
-
-    return scene[idx[:, 0],
-                 idx[:, 1],
-                 :]
+    # print("Dims of scene and indices",
+    #       (scene.size,
+    #       idx[:, 0] // 2,
+    #       idx[:, 1] // 2,))
+    return scene[:,
+                 idx[:, 0] // 2,
+                 idx[:, 1] // 2].transpose(0,1)
 
 if __name__=="__main__":
     scene = torch.randn(100, 200, 32)
-    ypred = torch.LongTensor(16,2).random_(0, 100)
-    print (get_scene(scene, ypred))
+    x_start = torch.randn(16,2)
+    ypred = torch.randn(16,2)
+    print (get_scene(scene, ypred, x_start))
+
