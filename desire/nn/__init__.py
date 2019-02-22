e7fe574abc7eea84ed9a5f5b5950440befaaa305
AkashGanesan <akaberto@gmail.com>
2019-02-21 23:04:45 -0500

    Fixed bug to import correct module.

--- desire/nn/__init__.py
+++ desire/nn/__init__.py
@@ -2,5 +2,5 @@ from .cvae import *
 from .rnn_decoder import *
 from .rnn_encoder import *
 from .social_pooling import SocialPool
-from .scene_pooling import PoolingCNNParams
+from .scene_pooling import ScenePoolingCNN
 from .scf import SCF
