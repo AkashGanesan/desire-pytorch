12e01d0074614b2ec5c3d22f066e45a5417005f2
AkashGanesan <akaberto@gmail.com>
2019-02-21 23:05:00 -0500

    Modified netowork to be the same as DESIRE.

--- desire/nn/scene_pooling.py
+++ desire/nn/scene_pooling.py
@@ -1,23 +1,41 @@
 import torch.nn as nn
-from desire.utils import PoolingCNNParams
+import torch.nn.functional as F
+import numpy as np
 
 
 class ScenePoolingCNN(nn.Module):
 
-    def __init__(self, params: PoolingCNNParams):
-        super(PoolingCNN, self).__init__()
-        self.MLP = nn.Sequential()
-        for i, (in_channel,
-                out_channel,
-                kernel_size,
-                stride,
-                nn_type,
-                activation) in enumerate(params.layer_configuration):
-            self.MLP.add_module(name="L%i" % (i), module=nn_type(in_channel,
-                                                                 out_channel,
-                                                                 kernel_size,
-                                                                 stride))
-            self.MLP.add_module(name="A%i" % (i), module=activation)
-            
+    def __init__(self):
+        super(ScenePoolingCNN, self).__init__()
+
+        self.pad1 = nn.ReflectionPad2d(tuple(map(lambda x: int(np.ceil(x)),
+                                                 ((0 + 5 - 2) / 2,
+                                                  (0 + 5 - 2) / 2,
+                                                  (0 + 5 - 2) / 2,
+                                                  (0 + 5 - 2) / 2))))
+
+        self.conv1 = nn.Conv2d(in_channels=3,
+                               out_channels=16,
+                               stride=2,
+                               kernel_size=(5, 5))
+
+        self.pad2 = nn.ReflectionPad2d((5 - 1) // 2)
+        self.conv2 = nn.Conv2d(in_channels=16,
+                               out_channels=32,
+                               stride=1,
+                               kernel_size=(5, 5))
+
+        self.pad3 = nn.ReflectionPad2d((5 - 1) // 2)
+        self.conv3 = nn.Conv2d(in_channels=32,
+                               out_channels=32,
+                               stride=1,
+                               kernel_size=(5, 5))
+
     def forward(self, x):
-        return self.MLP(x)
+        x = self.pad1(x)
+        x = F.relu(self.conv1(x))
+        x = self.pad2(x)
+        x = F.relu(self.conv2(x))
+        x = self.pad3(x)
+        x = F.relu(self.conv3(x))
+        return x
