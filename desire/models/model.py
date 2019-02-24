5907eaef178c792a21011ec9858175a6fe2dc75a
AkashGanesan <akaberto@gmail.com>
2019-02-23 23:45:59 -0500

    Fixed return types
    SGM now returns y_pred, x_enc_last_hidden and cvae's mean and logvar. Now,
    desire also returns all the parameters required for calculating the loss.

--- desire/models/model.py
+++ desire/models/model.py
@@ -5,29 +5,30 @@ from desire.utils import IOCParams, SGMParams
 import torch
 import torch.nn as nn
 
+
 class DESIRE(nn.Module):
 
     def __init__(self,
                  ioc_params: IOCParams,
                  sgm_params: SGMParams):
         super(DESIRE, self).__init__()
-        self.ioc_params = ioc_params
+
         self.sgm_params = sgm_params
-
-        self.IOC = IOC(ioc_params)
+        self.ioc_params = ioc_params
         self.SGM = SGM(sgm_params)
-
+        self.IOC = IOC(ioc_params)
     def forward(self,
                 x_rel,
                 y_rel,
                 x_start,
                 scene):
-        y_pred_rel, x_last_hidden = self.SGM(x_rel, y_rel)
+        y_pred_rel, x_last_hidden, mean, log_var = self.SGM(x_rel, y_rel)
+        # print("YPRED REL", y_pred_rel.shape)
         out_scores, pred_delta = self.IOC(y_pred_rel,
                                           x_last_hidden,
                                           scene,
                                           x_start)
-        return ypred, pred_delta
+        return y_pred_rel, pred_delta, mean, log_var
 
     def inference(self, x):
         ypred, x_last_hidden = self.SGM.inference(x)
@@ -36,8 +37,11 @@ class DESIRE(nn.Module):
 
 if __name__ == "__main__":
     model = DESIRE(IOCParams(), SGMParams())
-    x_rel = torch.rand(16, 2, 40)
-    y_rel = torch.rand(16, 2, 40)
-    x_start = torch.rand(16,2)
-    scene = torch.randn(640, 480, 3)
-    ypred, x_last_hidden, out_scores, last_hidden = model(x_rel, y_rel, x_start, scene)
+    x_rel = torch.rand(4, 2, 8)
+    y_rel = torch.rand(4, 2, 12)
+    x_start = torch.rand(4, 2)
+    scene = torch.randn(1, 3, 640, 480)
+    y_pred_rel, pred_delta, mean, log_var = model(x_rel,
+                                                  y_rel,
+                                                  x_start,
+                                                  scene)
