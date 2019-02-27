Fixed gradient flow issue
Gradient was calculated twice causing an in-place replacement and this one fixes
it.

desire/models/IOC.py
@@ -19,22 +19,34 @@ class IOC(nn.Module):
         self.scfs = []
         self.grus = []
         self.scoring_fcs = []
-        for i in range(self.params.num_layers):
-            self.grus.append(nn.GRU(**params.gru_params))
-            self.scfs.append(SCF(i, params.scf_params))
-            self.scoring_fcs.append(get_fc_act(params.scoring_fc))
+        # for i in range(self.params.num_layers):
+        #     self.grus.append(nn.GRU(**params.gru_params))
+        #     self.scfs.append(SCF(i, params.scf_params))
+        #     self.scoring_fcs.append(get_fc_act(params.scoring_fc))
+        self.grus = nn.ModuleList([nn.GRU(**params.gru_params) for i in range(params.num_layers)])
+        self.scfs = nn.ModuleList([SCF(i, params.scf_params) for i in range(params.num_layers)])
+        self.scoring_fcs = nn.ModuleList([get_fc_act(params.scoring_fc) for i in range(params.num_layers)])
+
         self.last_hidden_to_delta = nn.Linear(params.gru_params['hidden_size'],
                                               (params.num_layers
-                                              * params.num_dims))
+                                               * params.num_dims))
 
         self.scene_pooling_cnn = ScenePoolingCNN()
 
     def forward(self, ypred, prev_hidden, scene, x_start):
-        velocity = np.gradient(ypred.detach().numpy(), axis=0)
+        ypred_cpu = ypred.cpu()
+        ypred_cpu = ypred_cpu.detach().numpy()
+        velocity = np.gradient(ypred_cpu, axis=0)
+        velocity = torch.Tensor(velocity).to(ypred.device)
         prev_hidden = prev_hidden.unsqueeze(0)
         out_scores = []
         scene = self.scene_pooling_cnn(scene).squeeze(0)
         # print("Scene shape is", scene.size())
+        
+        prev_hidden = prev_hidden.clone()
+        prev_hidden.detach()
+        
+        
         for i in range(self.params.num_layers):
             prev_hidden.squeeze_(0)
             # print ("prev_hidden shape", prev_hidden.shape,
@@ -42,11 +54,19 @@ class IOC(nn.Module):
             #        velocity.shape,
             #        scene.shape,
             #        x_start.shape)
+
+            # print("prev_hidden", prev_hidden.get_device())
+            # print("ypred", ypred.get_device())
+            # print("velocity", velocity.get_device())
+            # print("scene", scene.get_device())
+            # print("x_start", x_start.get_device())
+
             scf_out = self.scfs[i](prev_hidden,
                                    ypred[:, :, i],
                                    velocity[:, :, i],
                                    scene,
                                    x_start)
+            # print("scf_out", scf_out.get_device())
             # print("scf dimensions", scf_out.size())
             gru_out, prev_hidden = self.grus[0](scf_out.unsqueeze(1),
                                                 prev_hidden.unsqueeze(0))
@@ -61,9 +81,10 @@ class IOC(nn.Module):
 
 
 if __name__ == "__main__":
-    model = IOC(IOCParams())
-    prev_hidden = torch.randn(16, 48)
-    ypred = torch.randn(16, 2, 40)
-    scene = torch.randn(1, 3, 640, 480)
-    x_start = torch.rand(16, 2)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = IOC(IOCParams()).to(device)
+    prev_hidden = torch.randn(16, 48).to(device)
+    ypred = torch.randn(16, 2, 40).to(device)
+    scene = torch.randn(1, 3, 640, 480).to(device)
+    x_start = torch.rand(16, 2).to(device)
     out_scores, prev_hidden = model(ypred, prev_hidden, scene, x_start)

desire/models/SGM.py
@@ -19,7 +19,7 @@ class SGM(nn.Module):
         self.dec = DecoderRNN(params.rnn_dec_params)
         self.dec_fc = nn.Linear(params.rnn_dec_params.gru_hidden_size,
                                 params.final_output_size)
-
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     def forward(self, x, y):
         x_enc_out, x_enc_hidden = self.enc_x(x)
         y_enc_out, y_enc_hidden = self.enc_y(y)
@@ -33,7 +33,7 @@ class SGM(nn.Module):
         masked_out = torch.cat([masked_out,
                                torch.zeros(batch_size,
                                            self.params.rnn_dec_params.n_layers - 1,
-                                           hidden_size)], dim=1)
+                                           hidden_size).to(self.device)], dim=1)
         dec_out, dec_hidden = self.dec(masked_out, hidden_rnn_dec_input)
         dec_out.transpose_(1, 2)  # Swap seq_length with no of dimensions
         dec_out_list = []
@@ -55,11 +55,12 @@ class SGM(nn.Module):
 
 
 if __name__ == "__main__":
-    model = SGM(SGMParams())
-    x = torch.rand(16, 2, 8)
-    y = torch.rand(16, 2, 12)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SGM(SGMParams()).to(device)
+    x = torch.rand(16, 2, 8).to(device)
+    y = torch.rand(16, 2, 12).to(device)
     pred, last_hidden, means, log_var = model(x, y)
-
+    log_var.sum().backward()
 
 
 # x_enc_out, x_enc_hidden = model.SGM.enc_x(x_rel)

desire/nn/loss.py
@@ -44,7 +44,7 @@ def l2_loss(pred_traj, pred_traj_gt, mode='sum'):
 
 def kld_loss(mean,
              log_var):
-    KLD = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp(), dim=1)
+    KLD = - 0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp(), dim=1)
     return KLD
 
 

desire/nn/scf.py
@@ -17,7 +17,13 @@ class SCF(nn.Module):
     def forward(self, hidden, ypred, velocity, scene, x_start):
 
         vel_out = self.velocity_fc(ypred)
+        # print(ypred.device,
+        #       x_start.device,
+        #       hidden.device,
+        #       vel_out.device)
         scene_out = get_scene(scene, ypred, x_start, self.params.scene_size)
+
+        # print("scene out device", scene_out.device)
         sp_out = self.sp_nn(ypred, x_start, hidden)
         # print("Shapes 1", ypred.shape, x_start.shape, scene.shape)
         # print ("Shapes",
@@ -32,10 +38,15 @@ if __name__ == "__main__":
     num_agents = 4
     dimensions = 2
     length = 12
-    scf = SCF(idx, SCFParams())
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    scf = SCF(idx, SCFParams()).to(device)
+    print(scf)
     y = torch.randn(num_agents, dimensions)
-    x_start = torch.randn(num_agents, dimensions)
-    v = np.gradient(y, axis=0)
-    hidden = torch.randn(num_agents, 48)
-    scene = torch.randn(1, 32, 100, 120)
+    x_start = torch.randn(num_agents, dimensions).to(device)
+    v = torch.Tensor(np.gradient(y, axis=0)).to(device)
+    y = y.to(device)
+    hidden = torch.randn(num_agents, 48).to(device)
+    scene = torch.randn(32, 720 // 2, 576 // 2).to(device)
+    
     m = scf(hidden, y, v, scene, x_start)
+    

desire/nn/social_pooling.py
@@ -70,16 +70,17 @@ class SocialPool(nn.Module):
         y_pred = y_pred_rel + x_start
         ridx, widx = ring_indices(y_pred,
                                   self.params)
+
         valid_hidden_idx = pool_layers(ridx,
                                        widx,
                                        self.params)
 
-
         num_agents = y_pred_rel.size(0)
+
         log_polar_hidden = torch.zeros(num_agents,
                                        self.params.num_rings,
                                        self.params.num_wedges,
-                                       self.params.hidden_size)
+                                       self.params.hidden_size).to(x_start.device)
 
 
         for (k, v) in valid_hidden_idx.items():
@@ -90,8 +91,8 @@ class SocialPool(nn.Module):
             # print("log polar hidden shape", hidden.shape)
             log_polar_hidden[k] = hidden[v].mean(dim=0)
 
-        
         log_polar_hidden = log_polar_hidden.flatten(start_dim=1, end_dim=-1)
+        # print("log_polar_hidden ", log_polar_hidden.device)
         return F.relu(self.fc(log_polar_hidden))
 
 

desire/utils/seq_net.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+import torch
 
 def return_seq(params):
     seq_net = nn.Sequential()
@@ -17,7 +17,7 @@ def return_seq(params):
         if activation is not None:
             self.seq_net.add_module(name="A%i" % (i),
                                     module=activation)
-    return seq_net
+    return seq_net.to
 
 def get_fc_act(layers):
     seq_net = nn.Sequential()

scripts/train.py
@@ -13,7 +13,7 @@ from desire.utils.misc import relative_to_abs, get_dset_path
 from desire.utils.misc import int_tuple, bool_flag, get_total_norm
 from desire.models import DESIRE
 from desire.utils.params import IOCParams, SGMParams
-from desire.nn.loss import total_loss
+from desire.nn.loss import *
 from PIL import Image
 
 
@@ -41,10 +41,15 @@ def main(dataset_name,
     # logger.info("Initializing val dataset")
     # _, val_loader = data_loader(val_path)
 
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.info("Device is %s", device)
+
     image = Image.open(path_of_static_image)
     scene = TF.to_tensor(image)
     scene.unsqueeze_(0)
-
+    scene = scene.to(device)
+    
     iterations_per_epoch = len(train_dset) / batch_size
     if num_epochs:
         num_iterations = int(iterations_per_epoch * num_epochs)
@@ -55,6 +60,7 @@ def main(dataset_name,
 
     desire = DESIRE(IOCParams(),
                     SGMParams())
+    desire = desire.to(device)
     lr = 1e-3
     optimizer = optim.Adam(desire.parameters(),lr=lr)
 
@@ -65,7 +71,6 @@ def main(dataset_name,
 
         desire.load_state_dict(restore_dict)
 
-
     curr_epoch = 0
     t = 0
     print("Num iterations", num_iterations)
@@ -73,32 +78,41 @@ def main(dataset_name,
         for batch in train_dset:
             optimizer.zero_grad()
             obs_traj, pred_traj, obs_traj_rel, pred_traj_rel, _, _= batch
+            obs_traj = obs_traj.to(device)
+            obs_traj_rel = obs_traj_rel.to(device)
+            pred_traj = pred_traj.to(device)
+            pred_traj_rel = pred_traj_rel.to(device)
             # print("obs_traj size", obs_traj.size())
             # print("pred_traj size", pred_traj.size())
             # print("obs_traj_rel size", obs_traj_rel.size())
             # print("pred_traj_rel size", pred_traj_rel.size())
+            # logging.info("scene device id %s", scene.get_device())
+            # logging.info("OBS_TRAJ device id: %s", obs_traj.get_device())
+            x_start = obs_traj[:, :, 0].clone().to(device)
+            # logging.info("x_start device id: %s", x_start.get_device())
+            y_pred_traj, pred_delta, mean, log_var = desire(obs_traj_rel,
+                                                            pred_traj_rel,
+                                                            x_start,
+                                                            scene)
 
-            x_start = obs_traj[:, :, 0]
-            y_pred_traj, pred_delta, mean, log_var = desire.forward(obs_traj_rel,
-                                                                    pred_traj_rel,
-                                                                    x_start,
-                                                                    scene)
+
             tloss, all_loss = total_loss(y_pred_traj,
                                          pred_delta,
                                          pred_traj,
                                          mean,
                                          log_var)
-            
+
             tloss.backward()
             optimizer.step()
-            if t % 10 == 0:
-                logging.info("all_loss %s", str(all_loss))
+            if t % 1000 == 0:
+                logging.info("Total loss %s", str(tloss))
                 break
             t +=1
 
 
 
 if __name__ == "__main__":
-    dataset_name = "/home/akash/learn/mypage/desire-torch/dataset/datasets/zara1/"
-    path_of_static_image = "/home/akash/learn/mypage/desire-torch/zara01.background.png"
+    print(os.getcwd())
+    dataset_name = os.path.abspath("./dataset/datasets/zara1/")
+    path_of_static_image = os.path.abspath("./zara01.background.png")
     a, b = main(dataset_name, path_of_static_image)